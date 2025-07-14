071ffaf28c90889d8d2997c75b5053b5213a627a
V1va1di <h0me6.9w0rk0@gmail.com>
2025-07-14 18:16:31 +0400

    Урок №4. float, int и арифметические операции
    Задание №1
    
    Пользователь вводит стороны прямоугольника, выведите его площадь и периметр. На вход программе могут подаваться как целые числа, так и вещественные

---
+++ TasksSynergy 4-1.py
@@ -0,0 +1,6 @@
+a = float(input("Длина стороны A:"))
+b = float(input("Длину стороны B:"))
+area = a * b
+perimeter = 2 * (a + b)
+print("Площадь прямоугольника:", area)
+print("Периметр прямоугольника:", perimeter)
